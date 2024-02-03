38784a576aa07167fad21edc1bf71016aaebf0e0
ussenuk <ukimanuka@gmail.com>
2024-02-03 12:46:41 +0300

    worked on file input output

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as text_file:
+        text_file.write(file_content)
+    # text_file = open(file_name, mode='w', encoding='utf-8')
+    # text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+        with open(f'{file_name}.txt', encoding='utf-8') as text_file:
+            return text_file.read()
+
+
+
+# write_file(file_name='lib/logfile.txt', file_content ="Log 1: 5 bananas added \n")
+# append_file(file_name='lib/logfile.txt', append_content ="Log 2: 3 bananas subtracted \n")
+# read_file(file_name='lib/logfile.txt')
